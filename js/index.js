1f1119af1cdd34d612a4be49abbd7f8f2114a164
jopfre <jonah.freeland@gmail.com>
2018-10-24 11:45:20 +0100

    removed plugins

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+
+	MicroModal.init();
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    // nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+    }
+})
+
+});
